9b3738614ae11e42498c4c50c4d8f4b5c5a77c29
Qvocha <pineappletoe@gmail.com>
2018-12-27 16:49:13 +0300

    constructors and getters
    realize contructor from incident and adjacency matrix.
    realize getter for vertex and edges number.

--- Graph/Graph/graph.cpp
+++ Graph/Graph/graph.cpp
@@ -4,16 +4,25 @@ using namespace std;
 
 Graph::Graph(std::vector<std::vector<int>> const & adjacencyMatrix)
 {
+	this->adjacencyMatrix = adjacencyMatrix;
+	adjancencyMatrixToIncidence();
 }
 Graph::Graph(std::vector<std::vector<int>> const & incidenceMatrix)
-{}
+{
+	this->incidenceMatrix = incidenceMatrix;
+	incidenceMatrixToAdjancency();
+}
 Graph::~Graph()
 {}
 
 int Graph::getNumberOfVertexes() const
-{}
+{
+	return adjacencyMatrix.size();
+}
 int Graph::getNumberOfEdges() const
-{}
+{
+	return incidenceMatrix[0].size();
+}
 
 void Graph::adjancencyMatrixToIncidence()
 {}
